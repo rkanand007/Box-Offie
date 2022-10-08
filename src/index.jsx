6d8b4afc590a2fcd3839f7b859b1620f22f2a4d0
rkanand007 <ramkrishananand7@gmail.com>
2022-10-08 15:16:06 +0530

    Updated package.json

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 //import reportWebVitals from './reportWebVitals';
 
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
 
