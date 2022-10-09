44110f3c5cbd904a352a58a3215f24d6e70612ec
rkanand007 <ramkrishananand7@gmail.com>
2022-10-09 14:22:49 +0530

    added React Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Switch>
-      <Route path="/">
+      <Route exact path="/">
         This is home <h1>Hello world</h1>{' '}
+      </Route>
+
+      <Route exact path="/started">
+        This is started <h1>Hello world</h1>{' '}
+      </Route>
+
+      <Route>
+        {' '}
+        <h1>This is 404 page</h1>
       </Route>
     </Switch>
   );
