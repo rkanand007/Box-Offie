0505aca95ef46ad4cfb26bb041cedd639b91e25b
rkanand007 <ramkrishananand7@gmail.com>
2022-10-17 21:05:15 +0530

    install styled component

--- src/components/actor/ActorCard.styled.jsx
+++ src/components/actor/ActorCard.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import { SearchCard } from '../styled';
 
-export const StyledShowCard = styled(SearchCard)`
+export const StyledActorCard = styled(SearchCard)`
   .deathday {
     margin: 0;
     margin-top: 15px;
